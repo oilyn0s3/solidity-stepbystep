78ea7bbc8f8f6ec5c36f8fa83b226d944e86b510
oilyn0s3 <oilyn0s3@gmail.com>
2022-06-18 23:16:22 +0530

    02-file done

--- 02_variables-Scope.sol
+++ 02_variables-Scope.sol
@@ -6,10 +6,29 @@ pragma solidity ^0.8.0;
         - local 
             + decalred inside a function
             + not stored on the blockchain
-
+        - state 
+            + decalred outside the function
+            + stored on the blockchain
+        - global
+            + global is global, it mostly provides information about 
+              the blockchain eg. msg.sender .
 */
 
 contract Variables {
 
+    // state variables
+    string public meSome = "aaaah";
+    uint public meNum = 69;
+
+    function doSomethingtoMe() public view { // kinky
+
+        // variables with local scope
+        uint magicNumber = 420; // notice there is no scope keywords like "public"
+
+        // some known global variables 
+        uint timestamp = block.timestamp; // current block timestamp
+        address sender = msg.sender; // adress of the contract caller 
+
+    }
 
 }
