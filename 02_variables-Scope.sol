7a4227333d38b45479f19dee737055e11d186cfa
oilyn0s3 <oilyn0s3@gmail.com>
2022-06-18 23:04:16 +0530

    2nd one of Variables

---
+++ 02_variables-Scope.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+/*
+    Variables can be decalared in 3 scopes:
+        - local 
+            + decalred inside a function
+            + not stored on the blockchain
+
+*/
+
+contract Variables {
+
+
+}
