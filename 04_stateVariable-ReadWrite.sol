83658d9a9b2094f31ee3d856dce8af5816270bd5
oilyn0s3 <oilyn0s3@gmail.com>
2022-06-19 23:29:27 +0530

    file 04 done

--- 04_stateVariable-ReadWrite.sol
+++ 04_stateVariable-ReadWrite.sol
@@ -3,16 +3,31 @@ pragma solidity ^0.8.0;
 
 contract SimpleStorage { // because it only reads and writes state variables
 
-// state variable to store the data through the function
-uint public magicNum; 
+    // state variable to store the data through the function
+    uint public magicNum; 
 
-/*
+    /*
 
-   The functions which change the value of a state variable are called 
-   setter functions. Calling a setter function initiates a transaction,
-   which costs gas. More the cumputation inside a setter function more gas it consumes.
+    The functions which change the value of a state variable are called 
+    setter functions. Calling a setter function initiates a transaction,
+    which costs gas. More the cumputation inside a setter function more gas it consumes.
 
-*/
+    */
 
+    function set(uint _magicNum) public { //setter fn
+        magicNum = _magicNum;
 
+    }
+
+    /*
+    
+    Getter functions just read the data from the blockchain, it does not edit the state of the 
+    blockchain. It just read the state variables without sending transactions hence not consuming
+    any gas. The have "view" visibility.
+    
+    */
+
+    function get() public view returns (uint) {
+        return magicNum;
+    }
 }
