1405ad0210e346fd894e59a12c5913ee3f54d889
oilyn0s3 <oilyn0s3@gmail.com>
2022-06-19 23:12:59 +0530

    file 04 init

---
+++ 04_stateVariable-ReadWrite.sol
@@ -0,0 +1,18 @@
+// SPDX-License_Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract SimpleStorage { // because it only reads and writes state variables
+
+// state variable to store the data through the function
+uint public magicNum; 
+
+/*
+
+   The functions which change the value of a state variable are called 
+   setter functions. Calling a setter function initiates a transaction,
+   which costs gas. More the cumputation inside a setter function more gas it consumes.
+
+*/
+
+
+}
