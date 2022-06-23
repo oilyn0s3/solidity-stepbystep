7a87204ac0e49e37af36ba17742d64049e81a46f
oilyn0s3 <oilyn0s3@gmail.com>
2022-06-23 23:47:41 +0530

    a lil intro

---
+++ 00_solidity-IntroMumboJumbo.sol
@@ -0,0 +1,17 @@
+/* 
+    Alright, If you were anything like me (if you were starting out in solidity and blockchain etc.), 
+    and looked at some solidity code the very fisrt thing that must have popped up in your mind would be 
+    - What the heck is a "contract" ?
+        Well a contract is sort of like a class, which has it's own set of functions and variables.
+        We refer them in such a way because, it is like a promise - an unbreakable promise - that 
+        resides on ethereum blockchain. Some times it can be referd as an "smart contract" it kind means the same 
+        i.e. a contract capable of running when some condition is met.
+    - Why do we need solidity?
+        Solidity is a high level, obejct oriented language used for creating smart contracts.
+        It's an special purpose programming language which is capable of running on EVM (Ethereum Virtual Machine).
+    - What is an EVM?
+        EVM or Ethereum Virtual Machine is 
+
+*/
+
+//
