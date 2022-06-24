547e5fb19f3e900cd908d2856f1223ff67eabd01
oilyn0s3 <oilyn0s3@gmail.com>
2022-06-24 23:56:10 +0530

    added theory for gas

--- 05_ether-wei-gas.sol
+++ 05_ether-wei-gas.sol
@@ -10,7 +10,6 @@ pragma solidity  ^0.8.0;
 
 */
 
-
 contract EtherCostMumboJumbo{
 
     /*
@@ -22,15 +21,22 @@ contract EtherCostMumboJumbo{
 
     uint public oneEther = 1 ether; // ether is also a unit and this will hold 1000000000000000000 as a value because 1 ether is made of 10^18 wei 
     bool public isOneEther = 1 ether == 1e18; // this would also be true 
-    
-
-
 
 }
 
 /*
     As I've mentioned above interaction and deployment of a contract requires some ether.
-    It is 
+    This amount is calculated by finding 
+        gas spent * gas price
+        + gas is the unit of computation
+        + gas spent is the total amount of gas used in a transaction
+        + gas price is how much ether you are willing to pay per gas 
+        higher the gas the higher the priority of being included in a block.
+
+    But there is limit to the amount of gas you can spend :
+        + gas limit -  max amount of gas you are willing to pay for the transaction (set by you)
+        + block gas limit - max amount of gas allowed in a block (set by the network)
+*/
 
 contract GasComputation {
 
