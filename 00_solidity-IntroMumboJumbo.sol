19b22acad76bd79145262f4ad318aeadc57ee037
oilyn0s3 <oilyn0s3@gmail.com>
2022-07-09 12:03:11 +0530

    File 00 done

--- 00_solidity-IntroMumboJumbo.sol
+++ 00_solidity-IntroMumboJumbo.sol
@@ -1,7 +1,20 @@
-/* 
+// SPDX-License-Identifier: UNLICENSED 
+
+pragma solidity ^0.8.0;
+/*
+    All solidity source code should start with a "version pragma" 
+    — a declaration of the version of the Solidity compiler this code should use. 
+    This is to prevent issues with future compiler versions potentially introducing
+    changes that would break your code.
+*/
+contract Intro {
+
+    /* 
     Alright, If you were anything like me (if you were starting out in solidity and blockchain etc.), 
     and looked at some solidity code the very fisrt thing that must have popped up in your mind would be 
     - What the heck is a "contract" ?
+        It is a fundamental building block of the Ethereum applications and all variables and functions 
+        beling to a contract.
         Well a contract is sort of like a class, which has it's own set of functions and variables.
         We refer them in such a way because, it is like a promise - an unbreakable promise - that 
         resides on ethereum blockchain. Some times it can be referd as an "smart contract" it kind means the same 
@@ -9,9 +22,7 @@
     - Why do we need solidity?
         Solidity is a high level, obejct oriented language used for creating smart contracts.
         It's an special purpose programming language which is capable of running on EVM (Ethereum Virtual Machine).
-    - What is an EVM?
-        EVM or Ethereum Virtual Machine is 
+    */
 
-*/
+}
 
-//
