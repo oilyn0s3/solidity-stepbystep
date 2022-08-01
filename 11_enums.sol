d0d3cf5f8ca5812b7a3c2fecce8165072d13628c
oilyn0s3 <oilyn0s3@gmail.com>
2022-08-01 20:30:40 +0530

    file 11 done

--- 11_enums.sol
+++ 11_enums.sol
@@ -35,7 +35,26 @@ contract Enum {
         Delivered      - 4
         Canceled       - 5
     */
+    
     OrderStutus public myOrder; // by default it is set to the first element i.e Processing
 
+    // accessing the order status
+    function getStatus() public view returns(OrderStutus) {
+        return myOrder;
+    }
 
+    // updating the order status
+    function updateStatus(OrderStutus _newStatus) public {
+        myOrder = _newStatus;
+    }
+
+    // we can also update the status by directly accessing the enum elements
+    function cancelOrder() public {
+        myOrder = OrderStutus.Canceled;
+    }
+
+    // deleting resets the enum to its first value i.e. 0
+    function resetOrder() public {
+        delete myOrder;
+    }
 }
